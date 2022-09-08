62f0e267ba547e0864c25b6f3a9a2f8ccde6205e
mark-baraka <barakaanderson24@gmail.com>
2022-09-08 15:31:57 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-/**
-  main - Entry Point
+/*
+ * main - Entry Point
  * Description: prints a string to the standard error
  * Return: Always 1(Error)
  */
 int main(void)
 {
 	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
-	write(2,  err, 59);
+	write(2,  err,);
 	return (1);
 }
