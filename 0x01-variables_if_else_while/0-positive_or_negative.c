622e69f4adf8ddd0d9690b47e9fe1b7732c8e4c2
mark-baraka <barakaanderson24@gmail.com>
2022-09-10 12:27:16 -0700

    m

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,11 +21,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	
 	if (n > 1)
 		printf("%d is positive\n", n);
+
 	else if (n < 0)
 		printf("%d is negative\n", n);
+
 	else
 		printf("%d is zero\n", n);
+	
 	return (0);
 }
