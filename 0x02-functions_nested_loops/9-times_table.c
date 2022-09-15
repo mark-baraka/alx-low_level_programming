759903e9f2f43bdc5b716b8ddd103ac17037184d
mark-baraka <barakaanderson24@gmail.com>
2022-09-15 14:28:33 -0700

    m

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,19 +6,14 @@
 void times_table(void)
 {
 	int a;
-
 	int b;
-
 	int c;
 
 	for (a = 0; a <= 9; a++)
-	{
-		for (b = 0; b <= 9; b++)
-		{
-			c = a * b;
+	{	for (b = 0; b <= 9; b++)
+		{		c = a * b;
 			if ((c / 10) == 0)
-			{
-				if (b == 0)
+			{		if (b == 0)
 				{
 					_putchar ('0');
 				}
