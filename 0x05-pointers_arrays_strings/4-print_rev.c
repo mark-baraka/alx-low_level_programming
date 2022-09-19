a8898cb35ce0310ab66fa399a31162cac3e55799
mark-baraka <barakaanderson24@gmail.com>
2022-09-19 14:10:54 -0700

    m

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,10 @@ void print_rev(char *s)
 
 {
 		int len = 0, index;
-
-	while (s[index++])
-	len++;
-	for (index = len - 1; index >= 0; index--)
-		putchar(s[index]);
-		putchar('\n');
+	
+		while (s[index++])
+			len++;
+			for (index = len - 1; index >= 0; index--)
+				putchar(s[index]);
+				putchar('\n');
 }
