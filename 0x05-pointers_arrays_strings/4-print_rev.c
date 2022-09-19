7d078c4922f78479864e8601a40c864f642954b8
mark-baraka <barakaanderson24@gmail.com>
2022-09-19 14:20:40 -0700

    m

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,22 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
- * print_rev - Prints a string in reverse.
- * s: The string to be printed.
+ * print_rev - prints a string in stdout in reverse
+ * s: string to print
+ * Return: void
  */
+
 void print_rev(char *s)
 
 {
-		int len = 0, index;
-	
-		while (s[index++])
-			len++;
-		for (index = len - 1; index >= 0; index--)
-			putchar(s[index]);
-		putchar('\n');
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	i = i - 1;
+	while (i >= 0)
+	{
+												putchar(*(s + i));
+												i--;
+											}
+	putchar('\n');
 }
